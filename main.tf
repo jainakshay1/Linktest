e2ae01aa3904f9fe19d5aae87efb99bb2c318470
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2024-08-05 16:21:18 +0000

    adding update 1

--- main.tf
+++ main.tf
@@ -1,30 +1,32 @@
 terraform {
   required_version = "1.9.3"
-
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
   
 }
 
 provider "azurerm" {
   features {}
-    client_id       = var.client_id 
-    client_secret   = var.client_secret
-    subscription_id = var.subscription_id
-    tenant_id       = var.tenant_id 
+    
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
-variable "client_id" {
+# variable "client_id" {
   
-}
-variable "client_secret" {
+# }
+# variable "client_secret" {
   
-}
-variable "subscription_id" {
+# }
+# variable "subscription_id" {
   
-}
-variable "tenant_id" {
+# }
+# variable "tenant_id" {
   
-}
+# }

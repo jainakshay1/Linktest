bbbec6698643b5ccd29767554958294caaab49a9
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2024-08-05 13:14:04 +0000

    adding vars

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 terraform {
   required_version = "1.9.3"
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
+  
 }
 
 provider "azurerm" {

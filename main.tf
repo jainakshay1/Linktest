01f3e15e0204e9aea0679e1956db6f71ec19c873
jainakshay1 <37770903+jainakshay1@users.noreply.github.com>
2024-08-05 11:43:27 +0000

    rg

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = "1.9.3"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
